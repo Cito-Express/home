83cd2a4c6ffc98d8efe6f89f1e178deb8bf07561
learnwithjacksun <codewithjacksun@gmail.com>
2025-07-18 02:20:04 +0100

    .

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -29,7 +29,7 @@ const Notificaiton = ({onClose}: {onClose: () => void}) => {
         <div data-aos="fade-down" className="h-[70px] relative bg-gradient-to-r from-transparent via-primary-1/10 to-transparent center">
             <div className="flex items-center gap-2 px-4 main">
                 <Gift size={20} className="text-primary-1"/>
-                <p className="text-sm font-medium text-primary-1">Get <strong>10%</strong> off your first order. <Link to="/" className="underline font-bold">Order Now</Link></p>
+                <p className="text-sm font-medium text-primary-1">Get <strong>10%</strong> off your first order. <Link to="/app" className="underline font-bold">Order Now</Link></p>
                 <button className="absolute right-4 top-2 text-primary-1" onClick={onClose}>
                     <X size={20}/>
                 </button>
